8149d5bc674600103302f95b6860275874708c99
AleGau00273 <alexandergaudet@yahoo.ca>
2025-11-30 15:46:06 -0400

    - Added Roll Dice Logic. - Completed One roll feature.

--- Lab5/Form1.cs
+++ Lab5/Form1.cs
@@ -6,20 +6,24 @@ namespace Lab5
         {
             InitializeComponent();
         }
-        /* Name:
+        /* Name: Alex Gaudet
          * Date: November 2025
          * This program rolls one dice or calculates mark stats.
          * Link to your repo in GitHub: 
          * */
 
         //class-level random object
+        //Declared here so that every method (Roll, Generate) can share it.
         Random rand = new Random();
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //select one roll radiobutton
+            radOneRoll.Checked = true;
 
             //add your name to end of form title
+            // += adds to the existing text ("Lab 5") instead of replacing it.
+            this.Text += " by Alex Gaudet";
 
         } // end form load
 
@@ -36,15 +40,23 @@ namespace Lab5
 
         private void btnRollDice_Click(object sender, EventArgs e)
         {
+            
             int dice1, dice2;
+
             //call ftn RollDice, placing returned number into integers
+            dice1 = RollDice();
+            dice2 = RollDice();
 
             //place integers into labels
+            lblDice1.Text = dice1.ToString();
+            lblDice2.Text = dice2.ToString();
 
             // call ftn GetName sending total and returning name
+            int total = dice1 + dice2;
+            string name = GetName(total);
 
             //display name in label
-
+            lblRollName.Text = name;
         }
 
         /* Name: ClearOneRoll
@@ -65,6 +77,13 @@ namespace Lab5
         * Return: integer (1-6)
         * Simulates rolling one dice */
 
+        private int RollDice()
+        {
+            //Generate a number between 1 and 6
+            // .Next(min, max) -> max is exclusive (so 7 means up to 6)
+            return rand.Next(1, 7);
+        }
+
 
         /* Name: GetName
         * Sent: 1 integer (total of dice1 and dice2) 
@@ -79,6 +98,42 @@ namespace Lab5
         *        11 = Yo-leven
         *        12 = Boxcars
         * Anything else = No special name*/
+
+        private string GetName(int total)
+        {
+            string name = "";
+
+            //Below is the switch statement to assign the name
+
+            switch (total)
+            {
+                case 2:
+                    name = "Snake Eyes";
+                    break;
+                case 3:
+                    name = "Little Joe";
+                    break;
+                case 5:
+                    name = "Fever";
+                    break;
+                case 7:
+                    name = "Most Common";
+                    break;
+                case 9:
+                    name = "Center Field";
+                    break;
+                case 11:
+                    name = "Yo-leven";
+                    break;
+                case 12:
+                    name = "Boxcars";
+                    break;
+                default:
+                    name = "No special name";
+                    break;
+            }
+            return name;
+        }
 
         private void btnSwapNumbers_Click(object sender, EventArgs e)
         {
